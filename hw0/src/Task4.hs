0a1981bfaa8d12647c7130f8c2f35b0fd26c0d71
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-02-11 01:42:52 +0300

    1.4 finished

--- hw0/src/Task4.hs
+++ hw0/src/Task4.hs
@@ -2,6 +2,8 @@ module Task4
        (
          factorial
        , iterateElement
+       , fibonacci
+       , mapFix
        ) where
 
 import Data.Function (fix)
@@ -20,3 +22,17 @@ iterateElement = fix preIterateElement
   where
     preIterateElement :: (a -> [a]) -> a -> [a]
     preIterateElement f x = x:(f x)
+
+fibonacci :: Integer -> Integer
+fibonacci = fix preFibonacci
+  where
+    preFibonacci :: (Integer -> Integer) -> Integer -> Integer
+    preFibonacci f n | n == 0 || n == 1 = 1
+                     | otherwise = f (n - 1) + f (n - 2)
+
+mapFix :: (a -> b) -> [a] -> [b]
+mapFix = fix preMapFix
+  where
+    preMapFix :: ((a -> b) -> [a] -> [b]) -> (a -> b) -> [a] -> [b]
+    preMapFix _ _ [] = []
+    preMapFix f mapper (x:xs) = (mapper x):(f mapper xs)
